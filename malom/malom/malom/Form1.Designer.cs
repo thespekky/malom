cff100cb76845cf6f5a3eb5fbc939acb0784b851
thespekky <bandras123456@gmail.com>
2021-11-24 09:37:25 +0100

    generálás kezdése

--- malom/malom/malom/Form1.Designer.cs
+++ malom/malom/malom/Form1.Designer.cs
@@ -29,13 +29,89 @@ namespace malom
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nev1txb = new System.Windows.Forms.TextBox();
+            this.nev2txb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gametablePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // nev1txb
+            // 
+            this.nev1txb.Location = new System.Drawing.Point(666, 68);
+            this.nev1txb.Name = "nev1txb";
+            this.nev1txb.Size = new System.Drawing.Size(100, 20);
+            this.nev1txb.TabIndex = 0;
+            // 
+            // nev2txb
+            // 
+            this.nev2txb.Location = new System.Drawing.Point(666, 111);
+            this.nev2txb.Name = "nev2txb";
+            this.nev2txb.Size = new System.Drawing.Size(100, 20);
+            this.nev2txb.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(585, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ELső név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(585, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Második név:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(657, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gametablePanel
+            // 
+            this.gametablePanel.Location = new System.Drawing.Point(38, 12);
+            this.gametablePanel.Name = "gametablePanel";
+            this.gametablePanel.Size = new System.Drawing.Size(520, 440);
+            this.gametablePanel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 459);
+            this.Controls.Add(this.gametablePanel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nev2txb);
+            this.Controls.Add(this.nev1txb);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox nev1txb;
+        private System.Windows.Forms.TextBox nev2txb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel gametablePanel;
     }
 }
 
--- malom/malom/malom/Form1.cs
+++ malom/malom/malom/Form1.cs
@@ -16,5 +16,54 @@ namespace malom
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tablageneralas();
+        }
+
+        private void tablageneralas()
+        {
+            int kozepi = 250;
+            int kozepj = 220;
+            PictureBox kep2 = new PictureBox();
+            kep2.Location = new System.Drawing.Point(kozepi,kozepj);
+            kep2.Visible = true;
+            kep2.BackColor = Color.Brown;
+            kep2.Size = new System.Drawing.Size(20, 20);
+            gametablePanel.Controls.Add(kep2);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    PictureBox kep = new PictureBox();
+                    if (j == 0)
+                    {
+                        kep.Location = new System.Drawing.Point(0 + (i * 250), 20 + (j * 50));
+                        kep.Name = j + "";
+                        kep.Visible = true;
+                        kep.BackColor = Color.Red;
+                        kep.Size = new System.Drawing.Size(20, 20);
+                    }
+                    if (j == 1)
+                    {
+                        kep.Location = new System.Drawing.Point(0 + (i * 250), 20 + (j * 50));
+                        kep.Name = j + "";
+                        kep.Visible = true;
+                        kep.BackColor = Color.Red;
+                        kep.Size = new System.Drawing.Size(20, 20);
+                    }
+
+
+                    gametablePanel.Controls.Add(kep);
+                }
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
